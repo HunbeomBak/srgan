661e4bad92fa6c0f46e5b055a11bd478f35f9a13
HunbeomBak <h2hhb0307@gmail.com>
2020-10-17 17:05:36 +0000

    add Dataset.py

---
+++ Dataset.py
@@ -0,0 +1,34 @@
+import os
+from PIL import Image
+import torch
+from torch.utils.data import Dataset
+from torchvision import transforms as T
+
+
+class sr_dataset(Dataset):
+    def __init__(self, HR_path, LR_path):
+        """
+        HR_path : path to high resolution path
+        LR_path : path to low resolution path
+        
+        """
+        
+        self.HR_list = [os.path.join(HR_path, i) for i in os.listdir(HR_path) if i.endswith('.png')]
+        self.HR_list.sort()
+        
+
+        self.LR_list = [os.path.join(HR_path, i) for i in os.listdir(LR_path) if i.endswith('.png')]
+        self.LR_list.sort()
+    
+        
+    def __len__(self):
+        return len(self.HR_list)
+    
+    def __getitem__(self, idx):
+        HR = Image.open(self.HR_list[idx])
+        LR = Image.open(self.LR_list[idx])
+        
+    
+        return {'HR' : T.ToTensor()(HR), 'LR' : T.ToTensor()(LR)}
+        
+ 
